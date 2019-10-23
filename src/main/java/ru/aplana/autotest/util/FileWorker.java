630bc3f6c33379ed675770ce6ab3da10ecf89c5f
Anastasiya-Ivanova97 <55584833+Anastasiya-Ivanova97@users.noreply.github.com>
2019-10-24 01:31:40 +0300

    Add files via upload

---
+++ src/main/java/ru/aplana/autotest/util/FileWorker.java
@@ -0,0 +1,38 @@
+package ru.aplana.autotest.util;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
+
+import static ru.aplana.autotest.pages.BasePage.products;
+
+public class FileWorker {
+
+    public static void writeIntoFile(String name) throws IOException {
+
+        File file = new File("src/main/resources/"+name);
+        FileWriter writer = new FileWriter(file);
+        for(Map.Entry<String, String> entry : products.entrySet()) {
+            String key = entry.getKey();
+            String value = entry.getValue();
+            writer.write(key+" = "+value.replaceAll("\\u20BD", "P")+"\n");
+        }
+        Collection<String> prices1 =  products.values();
+        List<String> prices = new ArrayList<>();
+        prices.addAll(prices1);
+        Collections.sort(prices);
+
+        Set<Map.Entry<String,String>> entrySet=products.entrySet();
+        String key = null;
+        String desiredObject=prices.get(prices.size()-1);//что хотим найти
+        for (Map.Entry<String,String> pair : entrySet) {
+            if (desiredObject.equalsIgnoreCase(pair.getValue())) {
+                key = pair.getKey();// нашли наше значение и возвращаем  ключ
+            }
+        }
+        writer.write("Товар с наибольшей ценой: "+key+" = "+products.get(key).replaceAll("\\u20BD", "P"));
+        writer.flush();
+        writer.close();
+    }
+}
