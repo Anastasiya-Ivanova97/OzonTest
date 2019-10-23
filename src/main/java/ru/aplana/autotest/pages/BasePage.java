054351d2dc9faa5f9cf03d75a53f72dd9b487bb7
Anastasiya-Ivanova97 <55584833+Anastasiya-Ivanova97@users.noreply.github.com>
2019-10-24 01:25:52 +0300

    Update BasePage.java

--- src/main/java/ru/aplana/autotest/pages/BasePage.java
+++ src/main/java/ru/aplana/autotest/pages/BasePage.java
@@ -1,8 +1,6 @@
 package ru.aplana.autotest.pages;
 
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Wait;
@@ -10,6 +8,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import ru.aplana.autotest.steps.BaseSteps;
 
 import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
 
 public class BasePage {
     WebDriver driver = BaseSteps.getDriver();
@@ -21,11 +20,41 @@ public class BasePage {
         wait = new WebDriverWait(driver,30);
     }
 
+    public boolean isElementPresent(By locator) {
+        try{
+            driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+            driver.findElement(locator).isDisplayed();
+            return true;
+        } catch (Exception e) {
+            driver.manage().timeouts().implicitlyWait(45,TimeUnit.SECONDS);
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean isElementPresent(WebElement element, By locator) {
+        try{
+            element.findElement(locator).isDisplayed();
+            driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+            return true;
+        } catch (Exception e) {
+            driver.manage().timeouts().implicitlyWait(45,TimeUnit.SECONDS);
+            e.printStackTrace();
+            return false;
+        }
+    }
+
     public void click(WebElement element) {
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(false);", element);
         Wait<WebDriver> wait = new WebDriverWait(driver, 60);
         wait.until(ExpectedConditions.elementToBeClickable(element));
         element.click();
+    }
+
+    public void input(WebElement element, String keys) {
+        element.click();
+        element.clear();
+        element.sendKeys(Keys.chord(keys));
     }
 
     public static String getProduct(String name) {
